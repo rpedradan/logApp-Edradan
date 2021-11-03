4f67b07802d2fc5b024188e86db5013cff8aaa2b
rpedradan <rpedradan19@gmail.com>
2021-11-03 17:18:48 +0800

    changed

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-	define('ROOT_URL', 'http://localhost/logApp-edradan/');
+	define('ROOT_URL', 'https://shrouded-hollows-82771.herokuapp.com/');
 	define('DB_HOST', 'sql6.freesqldatabase.com');
 	define('DB_USER', 'sql6447464');
 	define('DB_PASS', 'A1uHxaW3zs');
